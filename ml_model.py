5dfbfc0e9e0c1831e1d5495b14109a1334d6c598
himanshu100 <himanshu.bist@hotmail.com>
2022-01-04 19:49:28 +0530

    added 3 new ML model in code.

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("Adding all ML related code here.")
+
+
+def model1():
+    print("Adding code for ML Model 1.")
+
+
+def model2():
+    print("Adding code for ML Model 2.")
+
+
+def model3():
+    print("Adding code for ML Model 3.")
